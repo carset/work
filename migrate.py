c20b0e7b72b0e27060446034a3a7d37e11725319
carset <carset@163.com>
2016-03-24 15:55:58 +0800

    add migrate.py

--- migrate.py
+++ migrate.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python
 # -*- encoding:utf-8 -*-
+"""
+   数据迁移工具
+"""
+from app import app, db
+from flask_alembic import Alembic
 
+with app.app_context():
+    alembic = Alembic(app)
+    alembic.revision('some comments')
+    alembic.upgrade()
+
+
+# create: 15/11/30
 # End
-
